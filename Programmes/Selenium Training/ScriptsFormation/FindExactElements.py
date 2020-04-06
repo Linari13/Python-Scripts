6575f17f12e29ee71758f1f151c0ea00a27ce256
Linari <fred_p_64@hotmail.fr>
2020-04-06 10:43:55 +0200

    commiting changes in ScriptsFormation and new Python course

--- Programmes/Selenium Training/ScriptsFormation/FindExactElements.py
+++ Programmes/Selenium Training/ScriptsFormation/FindExactElements.py
@@ -7,12 +7,11 @@ import unittest                 #module for constructing and running tests
 from selenium import webdriver  #module specific to webpages testing
 
 class Test(unittest.TestCase):  #Creation class Test avec sousclasse unittest.TestCase
-
-    def test_exactFind( UrlWebPage = "https://s1.demo.opensourcecms.com/wordpress/", IdSearched = "search-form-1" , NameSearched = "s", TextSearched = "Hello world!"):
-    """This method will find several element by different attributes(Id, Name, Link Name)"""
+    def test_exactFind(self, UrlWebPage = "https://s1.demo.opensourcecms.com/wordpress/", IdSearched = "search-form-1" , NameSearched = "s", TextSearched = "Hello world!"):
+        """This method will find several element by different attributes(Id, Name, Link Name)"""
 
         driver = webdriver.Firefox()    #Setting browser
-        driver.get(UrlWebPage)           #Going to website tested
+        driver.get(UrlWebPage)          #Going to website tested
 
         """Search by ID"""
         print ('///////////////////////////////////////////////////////////////')
@@ -38,7 +37,7 @@ class Test(unittest.TestCase):  #Creation class Test avec sousclasse unittest.Te
         driver.close()
         pass
 
-    def test_missing(UrlWebPage = "https://s1.demo.opensourcecms.com/wordpress/", MissingTextSearched = "missing")
+    def test_missing(UrlWebPage = "https://s1.demo.opensourcecms.com/wordpress/", MissingTextSearched = "missind"):
 
         driver = webdriver.Firefox()    #Setting browser
         driver.get(UrlWebPage)          #Going to website tested
